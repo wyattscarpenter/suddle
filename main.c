e3b0624e360985800af5b9402606e27f443fd063
wyattscarpenter <wyattscarpenter@gmail.com>
2020-01-10 23:49:45 -0800

    make the game render an image

--- main.c
+++ main.c
@@ -3,26 +3,47 @@
 
 int main(int argc, char** argv){
 	puts("Hello.");
-	if (SDL_Init(SDL_INIT_VIDEO) != 0){
+	if (SDL_Init(SDL_INIT_VIDEO)){
 		printf("SDL_Init Error: %s", SDL_GetError());
 		return 1;
 	}
-	SDL_Window *window = SDL_CreateWindow("Hello World!", 100, 100, 640, 480, SDL_WINDOW_SHOWN);
+	//now we create the window, which I have cleverly set to take up my entire screen
+	SDL_Window *window = SDL_CreateWindow("Hello World!", 0, 0, 1920, 1080, SDL_WINDOW_SHOWN);
 	if (!window){
 		printf("SDL_CreateWindow Error: %s", SDL_GetError());
 		return 1;
 	}
+	SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
+	if (!renderer){
+		printf("SDL_CreateRendererError: %s", SDL_GetError());
+		return 1;
+	}
 	//Get window surface
-            SDL_Surface* screenSurface = SDL_GetWindowSurface( window );
-
-            //Fill the surface white
-            SDL_FillRect( screenSurface, NULL, SDL_MapRGB( screenSurface->format, 0xFF, 0xFF, 0xFF ) );
-            
-            //Update the surface
-            SDL_UpdateWindowSurface( window );
-
-            //Wait two seconds
-            SDL_Delay( 20000 );
+	SDL_Surface* screenSurface = SDL_GetWindowSurface(window);
+	//Fill the surface white
+	SDL_FillRect(screenSurface, NULL, SDL_MapRGB(screenSurface->format, 0xFF, 0xFF, 0xFF));
+	//Update the surface
+	SDL_UpdateWindowSurface(window);
+	//Wait second
+	SDL_Delay(1000);
+	SDL_Surface *testman = SDL_LoadBMP("test.bmp");
+	if(!testman){
+		printf("no testman :/ SDL_LoadBMP Error: %s", SDL_GetError());
+	}
+	SDL_Texture *texture = SDL_CreateTextureFromSurface(renderer, testman);
+	SDL_FreeSurface(testman);
+	if(!texture){
+		printf("SDL_CreateTextureFromSurface Error: %s", SDL_GetError());
+	}
+	for (int i = 0; i < 3; ++i){
+		//clear renderer
+		SDL_RenderClear(renderer);
+		//draw texture
+		SDL_RenderCopy(renderer, texture, NULL, NULL);
+		//update screen
+		SDL_RenderPresent(renderer);
+		SDL_Delay(1000);
+	}
 	puts("Goodbye.");
 	return 0;
 }
